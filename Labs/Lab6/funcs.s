617996e6bff576205454cc3e59d6e464e94e1322
j-ferber <ferberjustin5@gmail.com>
2023-03-05 19:46:02 -0500

    final lab 6 changes

--- Labs/Lab6/funcs.s
+++ Labs/Lab6/funcs.s
@@ -27,7 +27,7 @@
     pop %r10
     ret
  
-.globl _Z4add1Pyj # Question 3
+.globl _Z4add1Pyj # Question 3 (6, 4, 7, 8, 5, 10)
   _Z4add1Pyj:
     push %r11
     push %r10
@@ -45,7 +45,7 @@
     pop %r10
     ret    
 
-.globl _Z11replaceZeroy # Question 4
+.globl _Z11replaceZeroy # Question 4 (Binary:0000000000000000000000000000000011110010111011001011000010001110 -> Hex: f2ecb08e)
   _Z11replaceZeroy:
     xor %rax, %rax
     mov %rcx, %rax
@@ -54,7 +54,7 @@
     mov %rdx, %rcx
     ret
 
-.globl _Z10replaceOney # Question 5
+.globl _Z10replaceOney # Question 5 (Binary: 0000000000000000000000000000000011110010111011001011111011111110 -> Hex: f2ecbefe)
   _Z10replaceOney:
     xor %rax, %rax
     mov %rcx, %rax
@@ -63,15 +63,15 @@
     mov %rdx, %rcx
     ret
 
-.globl _Z7togglesy # Question 6          0xF2ECBABE = 1111 0010 1110 1100 1011 1010 1011 1110  
-  _Z7togglesy:     #                                = 1111 0010 1110 1100 1011 XXX0 1XXX 1110
-    xor %rax, %rax #                                = 1111 0010 1110 1100 1011 0100 1100 1110
+.globl _Z7togglesy # Question 6 (Binary: 0000000000000000000000000000000011110010111011001011111011111110 -> Hex: f2ecbefe) 
+  _Z7togglesy:     #                                
+    xor %rax, %rax #                                
     mov %rcx, %rax
     mov $0x00000440, %rdx
     xor %rdx, %rax
     ret
 
-.globl _Z8replaceDy # Question 7
+.globl _Z8replaceDy # Question 7 (Binary: 0000000000000000000010011000011101100101010000110010000111011001 -> Hex: 987654321d9)
   _Z8replaceDy:
     mov %rcx, %rax
     mov $0x000000000F0, %rdx
